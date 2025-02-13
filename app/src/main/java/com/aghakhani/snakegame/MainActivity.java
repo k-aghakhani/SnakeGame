84c291841ac81ba87b6bcc51658c7f1280be39dc
Kiarash Aghakhani <kiarash88ca@gmail.com>
2025-02-12 23:33:52 -0800

    Java code (MainActivity.java)

--- app/src/main/java/com/aghakhani/snakegame/MainActivity.java
+++ app/src/main/java/com/aghakhani/snakegame/MainActivity.java
@@ -8,17 +8,36 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
 public class MainActivity extends AppCompatActivity {
+
+    private GameView gameView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        // Initialize GameView and set it as the content view
+        gameView = new GameView(this);
+        setContentView(gameView);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (gameView != null) {
+            gameView.resume(); // Start the game loop
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (gameView != null) {
+            gameView.pause(); // Stop the game loop
+        }
     }
 }
